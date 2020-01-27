558a34e856a54ecc0d3240f6bce913182a297962
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:54:34 +0100

    fine

--- Dati_3/Program.cs
+++ Dati_3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Dati_3
 {
@@ -7,9 +9,30 @@ namespace Dati_3
         static void Main(string[] args)
         {
             Console.WriteLine("Nome del file sorgente: ");
-            string file = $"{ Console.ReadLine()}.txt";
+            string file = $"{ Console.ReadLine()}";
             Console.WriteLine("Nome del file nuovo: ");
-            string file2 = $"{ Console.ReadLine()}.txt";
+            string file2 = $"{ Console.ReadLine()}";
+            if (File.Exists(file))
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(file))
+                    {
+                        using (StreamWriter w = new StreamWriter(file2))
+                        {
+                            string line;
+                            while ((line = r.ReadLine()) != null)
+                            {
+                                w.WriteLine(line);
+                            }
+                        }
+                    }
+                } catch { }
+            }
+            else
+            {
+                Console.WriteLine("Il file sorgente non esiste");
+            }
         }
     }
 }
